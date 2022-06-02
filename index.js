9bca78723fb159933d72abe03f721a75de15bbfb
nick11967 <nick11967@snu.ac.kr>
2022-06-02 16:38:01 +0900

    crawling with pages1

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ fetch("data2.json")
 
 function appendData(data) {
   let mainContainer = document.getElementById("sportsData");
-  div.innerHTML = data.sports;
-  mainContainer.appendChild(div);
+  for(let i = 0; i < data.sports.length; i++){
+    let div = document.createElement("div");
+    div.innerHTML = i + ' ' + data.sports[i];
+    mainContainer.appendChild(div);
+  }
+  let mainContainer2 = document.getElementById("entertainsData");
+  for(let i = 0; i < data.entertains.length; i++){
+    let div = document.createElement("div");
+    div.innerHTML = data.entertains[i];
+    mainContainer2.appendChild(div);
+  }
 }
